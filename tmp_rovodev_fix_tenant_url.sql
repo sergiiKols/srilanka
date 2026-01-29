9f3ddd7e80134c698aff1037b68f70d41301aeca
sergiiKols <sergrodna@gmail.com>
2026-01-29 22:05:41 +0300

    Add personal map page with dynamic routing

--- tmp_rovodev_fix_tenant_url.sql
+++
@@ -1,9 +0,0 @@
--- Fix tenant URLs from localhost to production
-UPDATE tenants 
-SET personal_map_url = REPLACE(personal_map_url, 'http://localhost:4321', 'https://srilanka-37u2.vercel.app')
-WHERE personal_map_url LIKE 'http://localhost:4321%';
-
--- Verify the update
-SELECT id, telegram_user_id, personal_map_url 
-FROM tenants 
-WHERE telegram_user_id = 8311531873;
